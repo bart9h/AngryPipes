271cd9c9fe5311daa1ca1f4991bfba9f64436ede
Rodolfo Borges <rodolfo.borges@gmail.com>
2011-09-19 22:35:05 -0300

    android create project

---
+++ src/org/ninehells/pipe9/Pipe9.java
@@ -0,0 +1,15 @@
+package org.ninehells.pipe9;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Pipe9 extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
