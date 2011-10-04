3322be3885cc15d94ce108a23e5009e9d3a196e1
Rodolfo Borges <rodolfo.borges@gmail.com>
2011-10-04 19:05:06 -0300

    show game running time

--- src/org/ninehells/angrypipes/Game.java
+++ src/org/ninehells/angrypipes/Game.java
@@ -2,10 +2,13 @@ package org.ninehells.angrypipes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.ZoomControls;
 
 import us.gorges.viewaclue.TwoDScrollView;
@@ -37,6 +40,8 @@ public class Game extends Activity
 			}
 		});
 
+		mTimeLabel = new TextView(this);
+
 		ZoomControls zoomControls = new ZoomControls(this);
 		zoomControls.setOnZoomInClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -52,12 +57,15 @@ public class Game extends Activity
 		LinearLayout buttonBar = new LinearLayout(this);
 		buttonBar.addView(undoButton);
 		buttonBar.addView(zoomControls);
+		buttonBar.addView(mTimeLabel);
 
 		LinearLayout layout = new LinearLayout(this);
 		layout.setOrientation(LinearLayout.VERTICAL);
 		layout.addView(buttonBar);
 		layout.addView(scrollView);
 		setContentView(layout);
+
+		mTimerHandler.postDelayed(mTimerTask, 1000);
 	}
 
 	@Override
@@ -70,6 +78,26 @@ public class Game extends Activity
 
 	private Board mBoard;
 	private ViewBoard mBoardView;
+	private TextView mTimeLabel;
+
+	private long mTimerStart = SystemClock.elapsedRealtime() - 1000;
+	private Handler mTimerHandler = new Handler();
+	private Runnable mTimerTask = new Runnable() {
+		public void run() {
+			int seconds = (int)((SystemClock.elapsedRealtime() - mTimerStart)/1000);
+			int minutes = (int)(seconds/60);  seconds -= 60*minutes;
+			int hours   = (int)(minutes/60);  minutes -= 60*hours;
+			int days    = (int)(hours  /24);  hours   -= 24*days;
+
+			String s = "";
+			if (days  > 0) s += String.format("%dd ", days);
+			if (hours > 0) s += String.format("%dh ", hours);
+			s += String.format("%d:%02d", minutes, seconds);
+			mTimeLabel.setText(s);
+
+			mTimerHandler.postDelayed(this, 1000);
+		}
+	};
 }
 
 // vim600:fdm=syntax:fdn=2:nu:
